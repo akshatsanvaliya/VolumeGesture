b483696fd6fa947893ad40a267c9ad17a9380223
akshatsanvaliya <asanvaliya1@gmail.com>
2023-02-24 19:40:28 +0530

    commit

---
+++ GestureVolumeControl.py
@@ -0,0 +1,71 @@
+import cv2
+import time
+import numpy as np
+
+import HandTrackingModule as htm
+import math
+
+detector = htm.handDetector(detectionCon=0.8)
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+
+wCam, hCam = 640, 480
+
+cap = cv2.VideoCapture(0)
+cap.set(3, wCam)
+cap.set(4, hCam)
+pTime = 0
+
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(
+    IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = cast(interface, POINTER(IAudioEndpointVolume))
+volume.GetMute()
+volume.GetMasterVolumeLevel()
+volRange = volume.GetVolumeRange()
+volume.SetMasterVolumeLevel(0.0, None)
+minVol = volRange[0]
+maxVol = volRange[1]
+vol = 0
+volBar = 400
+while True:
+    success, img = cap.read()
+    detector.findhands(img)
+    lmList = detector.findPosition(img, draw=False)
+    if len(lmList) != 0:
+        # print(lmList[4], lmList[8])
+        x1, y1 = (lmList[4][1], lmList[4][2])
+        x2, y2 = (lmList[8][1], lmList[8][2])
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+
+        cv2.circle(img, (x1, y1), 15, (255, 0, 0), cv2.FILLED)
+        cv2.circle(img, (x2, y2), 15, (255, 0, 0), cv2.FILLED)
+        cv2.line(img, (x1, y1), (x2, y2), (255, 0, 0), 3)
+        cv2.circle(img, (cx, cy), 15, (255, 0, 0), cv2.FILLED)
+        length = math.hypot(x2 - x1, y2 - y1)
+        print(length)
+
+        # Hand Range is min= 50 and max= 300
+        # vol range (-96 - 0)
+
+        vol = np.interp(length, [0, 100], [minVol, maxVol])
+        volBar = np.interp(length, [0, 100], [400, 165])
+        print("Volume", int(length), " and ", vol)
+        volume.SetMasterVolumeLevel(vol, None)
+
+        if length < 250:
+            cv2.circle(img, (cx, cy), 15, (0, 255, 0), cv2.FILLED)
+    cv2.rectangle(img, (9, 163), (85, 400), (0, 255, 0), 3)
+    cv2.rectangle(img, (9, int(volBar)), (85, 400), (0, 255, 0), cv2.FILLED)
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+    cv2.putText(img, f'FPS:{int(fps)}', (50, 60), cv2.FONT_HERSHEY_COMPLEX,
+                1, (255, 0, 0), 3)
+    cv2.imshow("Img", img)
+    if cv2.waitKey(1) & 0xff == ord('Q'):
+        break
+cap.release()
+
+cv2.destroyAllWindows()
---
+++ HandTrackingModule.py
@@ -0,0 +1,70 @@
+import cv2
+import mediapipe as mp
+import time
+
+
+class handDetector():
+    def __init__(self, mode=False, maxHands=1, modelComplexity=1, detectionCon=0.5, trackCon=0.5):
+        self.mode = mode
+        self.maxHands = maxHands
+        self.modelComplex = modelComplexity
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.modelComplex,
+                                        self.detectionCon, self.trackCon)
+        self.mpDraw = mp.solutions.drawing_utils
+
+    def findhands(self, img, draw=True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.result = self.hands.process(imgRGB)
+        # print(result.multi_hand_landmarks)
+
+        if self.result.multi_hand_landmarks:
+            for handLms in self.result.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
+        return img
+
+    def findPosition(self, img, handNo=0, draw=True):
+
+        lmList = []
+        if self.result.multi_hand_landmarks:
+            myHand = self.result.multi_hand_landmarks[handNo]
+            for id, lm in enumerate(myHand.landmark):
+                # print(id, lm)
+                h, w, c = img.shape
+                px, py = int(lm.x * w), int(lm.y * h)
+                # print(id, px, py)
+                lmList.append([id, px, py])
+                if draw:
+                    cv2.circle(img, (px, py), 7, (255, 0, 0), cv2.FILLED)
+        return lmList
+
+
+def main():
+    pTime = 0
+    cTime = 0
+    cap = cv2.VideoCapture(0)
+    detector = handDetector()
+    while True:
+        success, img = cap.read()
+        img = detector.findhands(img)
+
+        lmList = detector.findPosition(img)
+        if len(lmList) != 0:
+            print(lmList[4])
+
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+
+        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3,
+                    (124, 252, 0), 3)
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
+
+
+if __name__ == "__main__":
+    main()
